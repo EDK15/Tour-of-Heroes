4d2cd974cafcd305bfa418e1dfa1b855e11784cf
EDK15 <85530652+EDK15@users.noreply.github.com>
2021-10-30 15:10:21 +0000

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'EDK' },
+  { id: 2, name: 'Saliou' },
+  { id: 3, name: 'Moussa' },
+  { id: 4, name: 'Khassi' },
+  { id: 5, name: 'Soda' },
+  { id: 6, name: 'Ousmane' },
+  { id: 7, name: 'Ousseynou' },
+  { id: 8, name: 'Khadim' },
+  { id: 9, name: 'Dimé' },
+  { id: 10, name: 'Tapha' }
+];
+
